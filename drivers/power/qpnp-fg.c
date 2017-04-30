707cf503098a4a08ce6ab2f2fb1f0ada12f942c7
Sultanxda <sultanxda@gmail.com>
2017-04-30 13:08:54 -0700

    power: qpnp-fg: Ignore FULL_SOC interrupt
    This isn't used in the OxygenOS kernel, so we probably shouldn't use it.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -591,6 +591,7 @@ struct fg_chip {
 	bool			fg_shutdown;
 	bool			use_soft_jeita_irq;
 	bool			allow_false_negative_isense;
+	bool			use_full_soc_irq;
 	struct delayed_work	update_jeita_setting;
 	struct delayed_work	update_sram_data;
 	struct delayed_work	update_temp_work;
@@ -1395,7 +1396,7 @@ static void fg_enable_irqs(struct fg_chip *chip, bool enable)
 	if (enable) {
 		enable_irq(chip->soc_irq[DELTA_SOC].irq);
 		enable_irq_wake(chip->soc_irq[DELTA_SOC].irq);
-		if (!chip->full_soc_irq_enabled) {
+		if (!chip->full_soc_irq_enabled && chip->use_full_soc_irq) {
 			enable_irq(chip->soc_irq[FULL_SOC].irq);
 			enable_irq_wake(chip->soc_irq[FULL_SOC].irq);
 			chip->full_soc_irq_enabled = true;
@@ -1414,7 +1415,7 @@ static void fg_enable_irqs(struct fg_chip *chip, bool enable)
 	} else {
 		disable_irq_wake(chip->soc_irq[DELTA_SOC].irq);
 		disable_irq_nosync(chip->soc_irq[DELTA_SOC].irq);
-		if (chip->full_soc_irq_enabled) {
+		if (chip->full_soc_irq_enabled && chip->use_full_soc_irq) {
 			disable_irq_wake(chip->soc_irq[FULL_SOC].irq);
 			disable_irq_nosync(chip->soc_irq[FULL_SOC].irq);
 			chip->full_soc_irq_enabled = false;
@@ -4130,7 +4131,7 @@ static void status_change_work(struct work_struct *work)
 			chip->vbat_low_irq_enabled = true;
 		}
 
-		if (!chip->full_soc_irq_enabled) {
+		if (!chip->full_soc_irq_enabled && chip->use_full_soc_irq) {
 			enable_irq(chip->soc_irq[FULL_SOC].irq);
 			enable_irq_wake(chip->soc_irq[FULL_SOC].irq);
 			chip->full_soc_irq_enabled = true;
@@ -4146,7 +4147,7 @@ static void status_change_work(struct work_struct *work)
 			chip->vbat_low_irq_enabled = false;
 		}
 
-		if (chip->full_soc_irq_enabled) {
+		if (chip->full_soc_irq_enabled && chip->use_full_soc_irq) {
 			disable_irq_wake(chip->soc_irq[FULL_SOC].irq);
 			disable_irq_nosync(chip->soc_irq[FULL_SOC].irq);
 			chip->full_soc_irq_enabled = false;
@@ -7458,6 +7459,9 @@ static int fg_of_init(struct fg_chip *chip)
 			chip->batt_info_restore, fg_batt_valid_ocv,
 			fg_batt_range_pct);
 
+	chip->use_full_soc_irq = of_property_read_bool(node,
+					"qcom,fg-use-full-soc-irq");
+
 	return rc;
 }
 
@@ -7497,11 +7501,13 @@ static int fg_init_irqs(struct fg_chip *chip)
 
 		switch (subtype) {
 		case FG_SOC:
-			chip->soc_irq[FULL_SOC].irq = spmi_get_irq_byname(
-					chip->spmi, spmi_resource, "full-soc");
-			if (chip->soc_irq[FULL_SOC].irq < 0) {
-				pr_err("Unable to get full-soc irq\n");
-				return rc;
+			if (chip->use_full_soc_irq) {
+				chip->soc_irq[FULL_SOC].irq = spmi_get_irq_byname(
+						chip->spmi, spmi_resource, "full-soc");
+				if (chip->soc_irq[FULL_SOC].irq < 0) {
+					pr_err("Unable to get full-soc irq\n");
+					return rc;
+				}
 			}
 			chip->soc_irq[EMPTY_SOC].irq = spmi_get_irq_byname(
 					chip->spmi, spmi_resource, "empty-soc");
@@ -7522,14 +7528,16 @@ static int fg_init_irqs(struct fg_chip *chip)
 				return rc;
 			}
 
-			rc = devm_request_irq(chip->dev,
-				chip->soc_irq[FULL_SOC].irq,
-				fg_soc_irq_handler, IRQF_TRIGGER_RISING,
-				"full-soc", chip);
-			if (rc < 0) {
-				pr_err("Can't request %d full-soc: %d\n",
-					chip->soc_irq[FULL_SOC].irq, rc);
-				return rc;
+			if (chip->use_full_soc_irq) {
+				rc = devm_request_irq(chip->dev,
+					chip->soc_irq[FULL_SOC].irq,
+					fg_soc_irq_handler, IRQF_TRIGGER_RISING,
+					"full-soc", chip);
+				if (rc < 0) {
+					pr_err("Can't request %d full-soc: %d\n",
+						chip->soc_irq[FULL_SOC].irq, rc);
+					return rc;
+				}
 			}
 			enable_irq_wake(chip->soc_irq[FULL_SOC].irq);
 			chip->full_soc_irq_enabled = true;
